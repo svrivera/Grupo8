2a42b2aa1f9de490d93bb10e9df9dd82aa20d54f
Sebastian Rivera <svrivera@uc.cl>
2017-06-27 16:44:39 -0400

    cambios init

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -17,7 +17,7 @@ def create_app():
 app = create_app()
 
 # REPLACE WITH YOUR DATABASE NAME
-MONGODATABASE = "myDatabase"
+MONGODATABASE = "dbName"
 MONGOSERVER = "localhost"
 MONGOPORT = 27017
 client = MongoClient(MONGOSERVER, MONGOPORT)
